5d38af36d5f3943ed600d442748f22c0987f1aab
fedeb123 <federicoab@gmail.com>
2025-10-02 20:26:35 -0300

    reestructurado de rutas

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+import Home from "./views/Home"
+
+export default function App() {
+  return (
+    <>
+      <Home></Home>
+    </>
+  )
+}
--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -1,7 +1,7 @@
-import { Header } from "./components/Header"
-import { Llamativo } from "./components/Llamativo"
-import { ProductoSeccion } from "./components/ProductoSeccion"
-import { Footer } from "./components/Footer"
+import { Header } from "../components/Header"
+import { Llamativo } from "../components/Llamativo"
+import { ProductoSeccion } from "../components/ProductoSeccion"
+import { Footer } from "../components/Footer"
 
 export default function Home() {
   return (
