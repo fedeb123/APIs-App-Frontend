406c5332f4a9b6b802c96d24ed5580c125960afd
fedeb123 <federicoab@gmail.com>
2025-10-12 23:49:43 -0300

    adicion de hook personalizado para simplificar el fetch

---
+++ client/src/hooks/useFetch.jsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+
+const apiUrl  = import.meta.env.VITE_APP_API_URL
+
+export default function useFetch(location, data = null, token = null) {
+    
+    const [loading, setLoading] = useState(true)
+    const [response, setResponse] = useState(null)
+    const [error, setError] = useState(null)
+
+    let options = {
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        }
+    }
+
+    const method = data ? 'POST' : 'GET'
+    const url = `${apiUrl}/${location}`
+
+    if (data) {
+        options.method = method
+        options.body = JSON.stringify(data)
+    }
+
+    if (token) {
+        options.Authorization = `Bearer ${token}`
+    }
+
+    useEffect(() => {
+        fetch(url, options)
+        .then((responseData) => responseData.json())
+        .then((responseJson) => {
+            setResponse(responseJson)
+            setLoading(false)
+        })
+        .catch((error) => {
+            setError(error)
+            setLoading(false)
+        })
+    }, [location, data, token])
+
+    return { response, loading, error}
+}
