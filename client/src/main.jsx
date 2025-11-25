710c16bf235d4e112ec69509650145216699bc7c
fedeb123 <federicoab@gmail.com>
2025-11-25 15:51:11 -0300

    adicion de Loading.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,20 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { AuthProvider } from './context/AuthContext.jsx'
 import { Provider } from 'react-redux'
 import './global.css'
 import App from './App.jsx'
-import { store } from './features/store.js'
+import store, { persistor } from './features/store.js'
+import { PersistGate } from 'redux-persist/integration/react'
+import Loading from './views/Loading.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <AuthProvider>
+      <PersistGate loading={<Loading />} persistor={persistor}>
+        <BrowserRouter>
           <App/>
-        </AuthProvider>
-      </BrowserRouter>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </StrictMode>
 )
