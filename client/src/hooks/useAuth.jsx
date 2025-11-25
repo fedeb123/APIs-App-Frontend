d8937eadfeb35a6424389a06a6f064d46480ab5b
fedeb123 <federicoab@gmail.com>
2025-11-25 15:18:50 -0300

    contextos y hooks reemplazados por redux

--- client/src/hooks/useAuth.jsx
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import { AuthContext } from "../context/AuthContext";
-
-
-// Este hook sirve para llamar al contexto y poder usarlo
-export default function useAuth() {
-    return useContext(AuthContext)
-}
